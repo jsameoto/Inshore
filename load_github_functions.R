8cd3a38502130c7902eb439bf85aac131fe1650c
Wilson <Brittany.Wilson@dfo-mpo.gc.ca>
2021-01-20 07:42:51 -0400

    Organized layout of Breakout_script.R
    Tidied script up a wee bit.

---
+++ load_github_functions.R
@@ -0,0 +1,13 @@
+#Work around RCurl package issue with getURL()
+
+funcs <- c("https://raw.githubusercontent.com/Mar-scal/Assessment_fns/master/Maps/convert_coords.R",
+           "https://raw.githubusercontent.com/Mar-scal/Assessment_fns/master/Maps/add_alpha_function.R",
+           "https://raw.githubusercontent.com/Mar-scal/Assessment_fns/master/Maps/pectinid_projector_sf.R")
+dir <- getwd()
+for(fun in funcs) 
+{
+  temp <- dir
+  download.file(fun,destfile = basename(fun))
+  source(paste0(dir,"/",basename(fun)))
+  file.remove(paste0(dir,"/",basename(fun)))
+}
