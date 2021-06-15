c3db1d524bb092806ef829f8b5b11914d73e2684
Wilson <Brittany.Wilson@dfo-mpo.gc.ca>
2021-06-15 07:38:10 -0300

    Re-structuring Inshore repo
    Copied over MeatweightShellHeight scripts location. Moved standardized_depth.R outside the BoF and SPA29W folder (shared script).

---
+++ BoF/Growth/SPA3_MeatWeightShellHeight_2020.R
@@ -0,0 +1,283 @@
+###........................................###
+###
+###    Meat weight/shell height modelling
+###    SPA3: 1996 to 2016
+###    L.Nasmith
+###    August 2015
+###  Mod. J.Sameoto Aug 2018, July 2019
+###........................................###
+#modified in July 2020 J.Sameoto 
+
+#required packages
+library(tidyverse)
+library(ROracle)
+library (lme4)
+library(lattice)
+
+# Define: 
+uid <- un.sameotoj
+pwd <- pw.sameotoj
+
+surveyyear <- 2019  #This is the last survey year for which you want to include  - not should match year of cruise below 
+cruise <- "BI2019"  #note should match year for surveyyear set above 
+
+assessmentyear <- 2020 #year in which you are conducting the survey 
+area <- "3"  #SPA assessing recall SPA 1A, 1B, and 4 are grouped; options: "1A1B4and5", "3", "6" 
+path.directory <- "Y:/INSHORE SCALLOP/BoF/"
+
+
+
+###
+# read in shell height and meat weight data from database
+###
+#strata.spa3<-c(22:24)
+
+#SQL query 1: detailed meat weight/shell height sampling data
+quer1 <- paste0("SELECT * FROM scallsur.scwgthgt WHERE strata_id IN (22,23,24) AND cruise = '", cruise, "'")
+
+
+#numbers by shell height bin
+quer2 <- paste0("SELECT * FROM scallsur.scliveres WHERE strata_id IN (22,23,24) AND cruise = '", cruise, "'")
+
+
+# ROracle; note this can take ~ 10 sec or so, don't panic
+chan <- dbConnect(dbDriver("Oracle"), username = uid, password = pwd,'ptran')
+
+
+# Select data from database; execute query with ROracle; numbers by shell height bin
+#detailed meat weight/shell height sampling data
+BIdetail.dat <- dbGetQuery(chan, quer1)
+
+#numbers by shell height bin
+BIlivefreq.dat <- dbGetQuery(chan, quer2)
+
+#add YEAR column to data
+BIdetail.dat$YEAR <- as.numeric(substr(BIdetail.dat$CRUISE,3,6))
+BIlivefreq.dat$YEAR <- as.numeric(substr(BIlivefreq.dat$CRUISE,3,6))
+
+
+###
+# read in depth information and add it to the meat weight/shell height dataframe
+###
+#there is no strata_id in Olex file to select on, need a unique identifier for tows to link to depth ($ID)
+
+BIdetail.dat$ID <- paste(BIdetail.dat$CRUISE,BIdetail.dat$TOW_NO,sep='.')
+BIlivefreq.dat$ID <- paste(BIlivefreq.dat$CRUISE,BIlivefreq.dat$TOW_NO,sep='.')
+detail.ID <- unique(BIdetail.dat$ID)
+livefreq.ID <- unique(BIlivefreq.dat$ID)
+
+OlexTows_all <- read.csv("Y:/INSHORE SCALLOP/BoF/StandardDepth/TowData/towsdd_StdDepth.csv")
+names(OlexTows_all)[which(colnames(OlexTows_all)=="RASTERVALU")] <- "OLEXDEPTH_M"   #rename "RASTERVALU" column
+OlexTows_all$OLEXDEPTH_M[OlexTows_all$OLEXDEPTH_M==-9999] <- NA
+OlexTows_all$ID <- paste(OlexTows_all$CRUISE,OlexTows_all$TOW_NO,sep='.')
+OlexTows_detail <- subset(OlexTows_all, ID%in%detail.ID)
+OlexTows_livefreq <- subset(OlexTows_all, ID%in%livefreq.ID)
+
+BIdetail <- merge(BIdetail.dat,subset(OlexTows_detail,select=c("ID","OLEXDEPTH_M")), all.x=T)
+BIdetail$ADJ_DEPTH <- BIdetail$OLEXDEPTH_M
+BIdetail$ADJ_DEPTH[is.na(BIdetail$OLEXDEPTH_M)] <- -1*BIdetail$DEPTH[is.na(BIdetail$OLEXDEPTH_M)] #*-1 because DEPTH is positive
+
+BIlivefreq <- merge(BIlivefreq.dat,subset(OlexTows_livefreq,select=c("ID","OLEXDEPTH_M")), all.x=T)
+BIlivefreq$ADJ_DEPTH <- BIlivefreq$OLEXDEPTH_M
+BIlivefreq$ADJ_DEPTH[is.na(BIlivefreq$OLEXDEPTH_M)] <- -1*BIlivefreq$DEPTH[is.na(BIlivefreq$OLEXDEPTH_M)] #*-1 because DEPTH is positive
+
+
+###
+# check detail file for NAs (WET_MEAT_WEIGHT and HEIGHT) and positive depths
+###
+summary(BIdetail)
+BIdetail <- BIdetail[complete.cases(BIdetail[,c(which(names(BIdetail)=="WET_MEAT_WGT"))]),]  #remove NAs from WET_MEAT_WEIGHT
+BIdetail <- BIdetail[complete.cases(BIdetail[,c(which(names(BIdetail)=="HEIGHT"))]),]  #remove NAs from HEIGHT
+summary(BIdetail)
+
+#---- Meat weight shell height modelling ----
+
+#Subset for year
+BIdetailYYYY <- subset(BIdetail, YEAR==surveyyear)
+
+#create dataset for model
+test.data <- subset(BIdetailYYYY, HEIGHT>40)
+test.data$Log.HEIGHT <- log(test.data$HEIGHT)
+test.data$Log.HEIGHT.CTR <- test.data$Log.HEIGHT-mean(test.data$Log.HEIGHT)
+test.data$Log.DEPTH <- log(abs(test.data$ADJ_DEPTH)) #take abs to keep value positive
+test.data$Log.DEPTH.CTR <- test.data$Log.DEPTH-mean(test.data$Log.DEPTH)
+summary(test.data)
+
+#plot depths by tow
+png(paste0(path.directory, assessmentyear, "/Assessment/Figures/Growth/BItowdepth",surveyyear,".png")) #!!!DEFINE
+plot (ADJ_DEPTH~TOW_NO, data=test.data)
+dev.off()
+
+#run model
+MWTSHBI.YYYY <- glmer(WET_MEAT_WGT~Log.HEIGHT.CTR+Log.DEPTH.CTR+(Log.HEIGHT.CTR|TOW_NO),data=test.data,
+                    family=Gamma(link=log), na.action = na.omit)
+
+summary(MWTSHBI.YYYY)
+
+#diagnostics
+latt <- data.frame(test.data, res=residuals(MWTSHBI.YYYY,"pearson"),fit=fitted(MWTSHBI.YYYY)) 
+
+#Residuals vs fitted - full area 
+png(paste0(path.directory, assessmentyear, "/Assessment/Figures/Growth/MWTSHBI",surveyyear,".png"))
+plot(MWTSHBI.YYYY) 
+dev.off()
+
+#Plot of tow level residuals
+png(paste0(path.directory, assessmentyear, "/Assessment/Figures/Growth/MWTSHBI",surveyyear,"_towresid.png"))
+xyplot(res~fit|as.factor(TOW_NO),data=latt, xlab="fitted", ylab="residuals",
+       panel = function(x, y) {
+         panel.xyplot(x, y)
+         panel.abline(h=0)
+       })
+dev.off()
+
+#Plot of tow level fitted values 
+png(paste0(path.directory, assessmentyear, "/Assessment/Figures/Growth/MWTSHBI",surveyyear,"_towfit.png"))
+xyplot(WET_MEAT_WGT~fit|as.factor(TOW_NO),data=latt, xlab="fitted", ylab="Wet Meat weight")
+dev.off()
+
+#Construct data.frame similar to BIlivenfreq for weight per tow
+livefreqYYYY <- subset(BIlivefreq, YEAR==surveyyear)
+liveweightYYYY <- livefreqYYYY
+
+#create matrix of depths by tow to use in predict function
+Log.height.ctr <- log(seq(2.5, 197.5, by = 5)) - mean(test.data$Log.HEIGHT) #each shell height bin to predict on
+log.ctr.adj_depth <- log(abs(livefreqYYYY$ADJ_DEPTH)) - mean(test.data$Log.DEPTH) #depth by tow
+
+temp <- matrix(NA,dim(liveweightYYYY)[1],40)
+
+#Use random effects for tows in detail sample and fixed effects otherwise
+#Random effects for tows that were sampled for meat weight shell height; here ID tows that were sampled  
+random.pred <- (1:dim(liveweightYYYY)[1])[is.element(liveweightYYYY$TOW_NO,unique(test.data$TOW_NO))]
+
+#fixed effects for tows that weren't sampled for meat weight shell height; here ID tows that were NOT sampled   
+fixed.pred <- (1:dim(liveweightYYYY)[1])[!is.element(liveweightYYYY$TOW_NO,unique(test.data$TOW_NO))]
+
+#Predict using Random effects for tows that were sampled for meat weight shell height
+for(i in random.pred) temp[i,] <- as.vector(predict(MWTSHBI.YYYY,newdata=data.frame(Log.HEIGHT.CTR=Log.height.ctr,Log.DEPTH.CTR=rep(log.ctr.adj_depth[i] ,40),TOW_NO=liveweightYYYY$TOW_NO[i]),re.form=NULL,type="response"))
+
+#Predict using fixed effects for tows that weren't sampled for meat weight shell height
+for(i in fixed.pred) temp[i,] <- as.vector(predict(MWTSHBI.YYYY,newdata=data.frame(Log.HEIGHT.CTR=Log.height.ctr,Log.DEPTH.CTR=rep(log.ctr.adj_depth[i] ,40)),re.form=~0,type="response"))
+
+
+#multply temp matrix (weight) by live numbers to get weight/size bin
+liveweightYYYY[,grep("BIN_ID_0", colnames(liveweightYYYY)):grep("BIN_ID_195", colnames(liveweightYYYY))] <- temp*livefreqYYYY[,grep("BIN_ID_0", colnames(livefreqYYYY)):grep("BIN_ID_195", colnames(livefreqYYYY))]
+
+#export file for later analysis
+write.csv(liveweightYYYY, paste0(path.directory, assessmentyear, "/Assessment/Data/SurveyIndices/SPA",area,"/BIliveweight",surveyyear,".csv")) 
+
+rm(uid)
+rm(pwd)
+rm(un.sameotoj)
+rm(pw.sameotoj)
+
+#!!!Now save workspace as .RData object: e.g. BIgrowth2019.RData
+save.image(file = paste0(path.directory, assessmentyear, "/Assessment/Data/Growth/SPA",area,"/BIgrowth",surveyyear,".RData"))
+
+
+# ----  Condition for Spatial Map ----
+# Only calculates the point condition for the single year you modelled above (i.e. surveyyear that's set above) 
+
+livefreq.condition.spatial <- livefreqYYYY
+#adjust depth, must center on same mean as used in original model
+livefreq.condition.spatial$log.ctr.adj_depth <- log(abs(livefreq.condition.spatial$ADJ_DEPTH)) - mean(test.data$Log.DEPTH) 
+#subset to just those tows that were detailed sampled 
+livefreq.condition.spatial <- livefreq.condition.spatial[is.element(livefreq.condition.spatial$TOW_NO, unique(test.data$TOW_NO)),c("TOW_NO","START_LAT", "START_LONG", "ADJ_DEPTH","log.ctr.adj_depth")]
+livefreq.condition.spatial$YEAR <- surveyyear #add year to dataframe
+
+
+#Predict a meat weight for 100mm for just sampled tows; #re.form=NULL (all random effects included since only predicting on those tows that were sampled)
+
+livefreq.condition.spatial$Condition <- predict(MWTSHBI.YYYY,newdata=data.frame(Log.HEIGHT.CTR=rep(log(100), dim(livefreq.condition.spatial)[1])-mean(test.data$Log.HEIGHT),
+                                                                                Log.DEPTH.CTR=livefreq.condition.spatial$log.ctr.adj_depth, 
+                                                                                TOW_NO=livefreq.condition.spatial$TOW_NO),
+                                                re.form=NULL, type="response")
+
+head(livefreq.condition.spatial)
+
+#export for spatial plot later 
+write.csv(livefreq.condition.spatial, paste0(path.directory, assessmentyear, "/Assessment/Data/SurveyIndices/SPA",area,"/BIConditionforMap",surveyyear,".csv"))
+
+
+# ---- For Condition Time Series Figure ----
+
+#mean.depth <- read.csv('Y:/INSHORE SCALLOP/BoF/StandardDepth/BoFMeanDepths.csv') #File for the constant depth to predict on by area
+#DEFINE DEPTH:
+#SPA3 VMS IN Modelled Area (InVMSandSMB) = -47.63
+#SMB = -24.59
+#InsideVMS (BILU_InsideVMS) = -61.1
+#OutsideVMS = -89.71
+
+
+#Bring in file with depths by area, note some are by strata groups within area
+mean.depth <- read.csv('Y:/INSHORE SCALLOP/BoF/StandardDepth/BoFMeanDepths.csv')[ ,c("AREA", "MeanDepth_m")] #File for the constant depth to predict on by area
+unique(mean.depth$AREA)
+length(mean.depth$AREA)
+
+mean.depth.BI<- mean.depth[mean.depth$AREA %in% c("SaintMarysBay",
+                                                    "SPA3 OUT",
+                                                    "BILU_InsideVMS",
+                                                    "SPA3 VMS IN Modelled Area"),]
+dim(mean.depth.BI)[1] == 4
+unique(mean.depth.BI$AREA)
+
+#create name column that matches condition file: 
+mean.depth.BI$AREA_NAME <- NA
+mean.depth.BI$AREA_NAME[mean.depth.BI$AREA == "SaintMarysBay"] <- "SMB"
+mean.depth.BI$AREA_NAME[mean.depth.BI$AREA == "BILU_InsideVMS"] <- "InVMS"
+mean.depth.BI$AREA_NAME[mean.depth.BI$AREA == "SPA3 OUT"] <- "OutVMS"
+mean.depth.BI$AREA_NAME[mean.depth.BI$AREA == "SPA3 VMS IN Modelled Area"] <- "InVMS_SMB"
+
+#add year, condition column 
+mean.depth.BI$YEAR <- surveyyear
+mean.depth.BI$Condition <- NA
+
+for (i in 1:length(mean.depth.BI$AREA)){
+  mean.depth.BI$Condition[i] <- predict(MWTSHBI.YYYY, newdata = data.frame(Log.HEIGHT.CTR=log(100) - mean(test.data$Log.HEIGHT),
+                                                                            Log.DEPTH.CTR = log(abs(mean.depth.BI$MeanDepth_m[i])) - mean(test.data$Log.DEPTH)),
+                                         re.form = NA, type = "response")  
+}
+mean.depth.BI
+mean.depth.BI$Condition <- round(mean.depth.BI$Condition,3)
+
+
+#Import previous year condition file: 
+#note contains IN and OUT VMS strata condition; they don't vary much since the only predictor is depth and SH is constant at 100mm 
+BI.con.ts <- read.csv(paste0(path.directory, assessmentyear-1,"/Assessment/Data/SurveyIndices/SPA3/SPA3_ConditionTimeSeries.csv"))
+#BI.con.ts <- BI.con.ts[BI.con.ts$YEAR!=2019,]
+
+#update timeseries and write out new file: 
+BI.con.ts <- rbind(BI.con.ts %>% select(YEAR, STRATA, CONDITION), mean.depth.BI %>% select("YEAR",STRATA="AREA_NAME",CONDITION="Condition"))
+
+BI.con.ts <- BI.con.ts[order(BI.con.ts$STRATA, BI.con.ts$YEAR),]
+BI.con.ts
+
+write.csv(BI.con.ts, paste0(path.directory, assessmentyear, "/Assessment/Data/SurveyIndices/SPA",area,"/SPA3_ConditionTimeSeries.csv"))
+
+
+#... Plot Condition Time Series Figure:
+
+condition.ts.plot <- ggplot(BI.con.ts,aes(x=YEAR, y=CONDITION,group_by(STRATA), color=STRATA)) +  
+  geom_line(aes(linetype=STRATA)) + geom_point(aes(shape=STRATA)) + 
+#  scale_linetype_manual(values=c("dotted", "twodash")) + 
+#  scale_shape_manual(values=c(1,6)) + 
+#  scale_color_manual(values=c("black","gray34" )) + 
+  xlab("Year") + ylab("Condition (meat weight, g)") + theme_bw() #+ 
+#  theme(legend.position = c(.8, .99),
+#        legend.justification = c("left", "top"),
+#        legend.box.just = "left",
+#        legend.margin = margin(6, 6, 6, 6)) 
+condition.ts.plot
+
+#Export plot 
+png(paste0(path.directory, assessmentyear, "/Assessment/Figures/SPA3_conditionTrends.png" ))
+condition.ts.plot
+dev.off()
+
+
+
+
+
+
+
+
---
+++ BoF/Growth/SPA6_MeatWeightShellHeight_2020.r
@@ -0,0 +1,307 @@
+###........................................###
+###
+###    Meat weight/shell height modelling
+###    SPA6: 1997 to 2015
+###    J.Sameoto, modified from
+###    L.Nasmith
+###    August 2015
+###    Mod J.SAmeoto Oct 2017
+###   Mod J.SAmeoto Sept 2018
+###   Mod J.SAmeoto Sept 2019
+###   Mod J.Sameoto June 2020 
+###........................................###
+#modified in July 2020 J.Sameoto 
+# Note Starata IDs for SPA 6: strata.spa6<-c(30:32)
+
+options(stringsAsFactors=FALSE)
+
+#required packages
+library(tidyverse)
+library(ROracle)
+library (lme4)
+library(lattice)
+library(lubridate)
+
+# Define: 
+uid <- un.sameotoj
+pwd <- pw.sameotoj
+
+surveyyear <- 2019  #This is the last survey year for which you want to include  - not should match year of cruise below 
+cruise <- "GM2019"  #note should match year for surveyyear set above 
+
+assessmentyear <- 2020 #year in which you are conducting the survey 
+area <- "6"  #SPA assessing recall SPA 1A, 1B, and 4 are grouped; options: "1A1B4and5", "3", "6" 
+path.directory <- "Y:/INSHORE SCALLOP/BoF/"
+
+
+# ROracle; note this can take ~ 10 sec or so, don't panic
+chan <- dbConnect(dbDriver("Oracle"),username=uid, password=pwd,'ptran')
+
+# Set SQL query 
+quer1 <- paste0("SELECT * FROM scallsur.scwgthgt WHERE cruise = '", cruise, "'")
+
+# read in shell height and meat weight data from database
+# detailed meat weight/shell height sampling data
+GMdetail.dat <- dbGetQuery(chan, quer1)
+
+#numbers by shell height bin
+quer2 <- paste0("SELECT * FROM scallsur.scliveres WHERE cruise = '", cruise, "'")
+GMlivefreq.dat <- dbGetQuery(chan, quer2)
+
+#add YEAR column to data
+GMdetail.dat$YEAR <- year(GMdetail.dat$TOW_DATE)
+GMlivefreq.dat$YEAR <- as.numeric(substr(GMlivefreq.dat$CRUISE,3,6))
+
+#check
+table(GMdetail.dat$YEAR)
+table(GMlivefreq.dat$YEAR)
+
+
+# read in depth information and add it to the meat weight/shell height dataframe
+# there is no strata_id in Olex file to select on, need a unique identifier for tows to link to depth ($ID)
+GMdetail.dat$ID <- paste(GMdetail.dat$CRUISE, GMdetail.dat$TOW_NO,sep='.')
+uniqueID <- unique(GMdetail.dat$ID)
+
+OlexTows_all <- read.csv("Y:/INSHORE SCALLOP/BoF/StandardDepth/TowData/towsdd_StdDepth.csv")
+names(OlexTows_all)[which(colnames(OlexTows_all)=="RASTERVALU")] <- "OLEXDEPTH_M"   #rename "RASTERVALU" column
+OlexTows_all$OLEXDEPTH_M[OlexTows_all$OLEXDEPTH_M==-9999] <- NA
+OlexTows_all$ID <- paste(OlexTows_all$CRUISE,OlexTows_all$TOW_NO,sep='.')
+OlexTows_bof <- subset (OlexTows_all, ID%in%uniqueID)
+
+GMdetail <- merge(GMdetail.dat,subset(OlexTows_bof,select=c("ID","OLEXDEPTH_M")), all.x=T)
+GMdetail$ADJ_DEPTH <- GMdetail$OLEXDEPTH_M
+GMdetail$ADJ_DEPTH[is.na(GMdetail$OLEXDEPTH_M)] <- -1*GMdetail$DEPTH[is.na(GMdetail$OLEXDEPTH_M)] #*-1 because DEPTH is positive
+
+#add depth to the livefreq dataframe
+GMlivefreq.dat$ID <- paste(GMlivefreq.dat$CRUISE,GMlivefreq.dat$TOW_NO,sep='.')
+uniqueIDb <- unique(GMlivefreq.dat$ID)
+
+OlexTows_freq <- subset(OlexTows_all,ID%in%uniqueIDb)
+GMlivefreq <- merge(GMlivefreq.dat,subset(OlexTows_freq,select=c("ID","OLEXDEPTH_M")), all=T)
+GMlivefreq$ADJ_DEPTH <- GMlivefreq$OLEXDEPTH_M
+GMlivefreq$ADJ_DEPTH[is.na(GMlivefreq$OLEXDEPTH_M)] <- -1*GMlivefreq$DEPTH[is.na(GMlivefreq$OLEXDEPTH_M)] #*-1 because DEPTH is positive
+summary(GMlivefreq)
+
+
+# check detail file for NAs (WET_MEAT_WEIGHT and HEIGHT) and positive depths
+summary(GMdetail)
+GMdetail <- GMdetail[complete.cases(GMdetail[,c(which(names(GMdetail)=="WET_MEAT_WGT"))]),]  #remove NAs from WET_MEAT_WEIGHT
+GMdetail <- GMdetail[complete.cases(GMdetail[,c(which(names(GMdetail)=="HEIGHT"))]),]  #remove NAs from HEIGHT
+summary(GMdetail)
+
+
+#---- Meat weight shell height modelling ----
+
+#Subset for year
+GMdetail.foryear <- subset(GMdetail, YEAR==surveyyear)
+
+#create dataset for model
+test.data <- subset(GMdetail.foryear, HEIGHT>40)
+test.data$Log.HEIGHT <- log(test.data$HEIGHT)
+test.data$Log.HEIGHT.CTR <- test.data$Log.HEIGHT - mean(test.data$Log.HEIGHT)
+test.data$Log.DEPTH <- log(abs(test.data$ADJ_DEPTH)) #take abs to keep value positive
+test.data$Log.DEPTH.CTR <- test.data$Log.DEPTH - mean(test.data$Log.DEPTH)
+summary(test.data)
+
+#plot depths by tow
+png(paste0(path.directory, assessmentyear, "/Assessment/Figures/Growth/GMtowdepth",surveyyear,".png")) #!!!DEFINE
+plot(ADJ_DEPTH~TOW_NO, data=test.data)
+dev.off()
+
+#run model; update model name to correspond to year
+MWTSHGM.YYYY <- glmer(WET_MEAT_WGT~Log.HEIGHT.CTR+Log.DEPTH.CTR+(Log.HEIGHT.CTR|TOW_NO),data=test.data,
+                        family=Gamma(link=log), na.action = na.omit)
+
+summary(MWTSHGM.YYYY)
+
+#diagnostics
+latt <- data.frame(test.data, res=residuals(MWTSHGM.YYYY,"pearson"),fit=fitted(MWTSHGM.YYYY)) #update model name
+
+#Residuals vs fitted - full area 
+png(paste0(path.directory, assessmentyear, "/Assessment/Figures/Growth/MWTSHGM",surveyyear,".png"))
+plot(MWTSHGM.YYYY) #update model name
+dev.off()
+
+#Plot of tow level residuals
+png(paste0(path.directory, assessmentyear, "/Assessment/Figures/Growth/MWTSHGM",surveyyear,"_towresid.png"))
+xyplot(res~fit|as.factor(TOW_NO),data=latt, xlab="fitted", ylab="residuals",
+  panel = function(x, y) {
+         panel.xyplot(x, y)
+         panel.abline(h=0)
+       })
+dev.off()
+
+#Plot of tow level fitted values 
+png(paste0(path.directory, assessmentyear, "/Assessment/Figures/Growth/MWTSHGM",surveyyear,"_towfit.png"))
+xyplot(WET_MEAT_WGT~fit|as.factor(TOW_NO),data=latt, xlab="fitted", ylab="Wet Meat weight")
+dev.off()
+
+#Construct data.frame similar to GMlivenfreq for weight per tow
+livefreqYYYY <- subset(GMlivefreq, YEAR==surveyyear)
+liveweightYYYY <- livefreqYYYY
+
+#create matrix of depths by tow to use in predict function
+Log.height.ctr <- log(seq(2.5, 197.5, by = 5)) - mean(test.data$Log.HEIGHT) #each shell height bin to predict on
+log.ctr.adj_depth <- log(abs(livefreqYYYY$ADJ_DEPTH)) - mean(test.data$Log.DEPTH) #depth by tow
+
+temp <- matrix(NA,dim(liveweightYYYY)[1],40)
+
+#Use random effects for tows in detail sample and fixed effects otherwise
+#Random effects for tows that were sampled for meat weight shell height; here ID tows that were sampled  
+random.pred <- (1:dim(liveweightYYYY)[1])[is.element(liveweightYYYY$TOW_NO,unique(test.data$TOW_NO))]
+
+#fixed effects for tows that weren't sampled for meat weight shell height; here ID tows that were NOT sampled   
+fixed.pred <- (1:dim(liveweightYYYY)[1])[!is.element(liveweightYYYY$TOW_NO,unique(test.data$TOW_NO))]
+
+#Predict using Random effects for tows that were sampled for meat weight shell height
+for(i in random.pred) temp[i,] <- as.vector(predict(MWTSHGM.YYYY,newdata=data.frame(Log.HEIGHT.CTR=Log.height.ctr,Log.DEPTH.CTR=rep(log.ctr.adj_depth[i], 40),TOW_NO=liveweightYYYY$TOW_NO[i]),re.form=NULL,type="response"))
+
+#Predict using fixed effects for tows that weren't sampled for meat weight shell height
+for(i in fixed.pred) temp[i,] <- as.vector(predict(MWTSHGM.YYYY,newdata=data.frame(Log.HEIGHT.CTR=Log.height.ctr,Log.DEPTH.CTR=rep(log.ctr.adj_depth[i], 40)), re.form=~0,type="response"))
+
+
+#multply temp matrix (weight) by live numbers to get weight/size bin
+liveweightYYYY[,grep("BIN_ID_0", colnames(liveweightYYYY)):grep("BIN_ID_195", colnames(liveweightYYYY))] <- temp*livefreqYYYY[,grep("BIN_ID_0", colnames(livefreqYYYY)):grep("BIN_ID_195", colnames(livefreqYYYY))]
+
+#export file for later analysis
+write.csv(liveweightYYYY, paste0(path.directory, assessmentyear, "/Assessment/Data/SurveyIndices/SPA",area,"/GMliveweight",surveyyear,".csv")) 
+
+
+rm(uid)
+rm(pwd)
+rm(un.sameotoj)
+rm(pw.sameotoj)
+
+#!!!Now save workspace as .RData object: e.g. GMgrowth2019.RData
+save.image(file = paste0(path.directory, assessmentyear, "/Assessment/Data/Growth/SPA",area,"/GMgrowth",surveyyear,".RData"))
+
+
+
+# ----  Condition for Spatial Map ----
+# Only calculates the point condition for the single year you modelled above (i.e. surveyyear that's set above) 
+
+livefreq.condition.spatial <- livefreqYYYY
+#adjust depth, must center on same mean as used in original model
+livefreq.condition.spatial$log.ctr.adj_depth <- log(abs(livefreq.condition.spatial$ADJ_DEPTH)) - mean(test.data$Log.DEPTH) 
+#subset to just those tows that were detailed sampled 
+livefreq.condition.spatial <- livefreq.condition.spatial[is.element(livefreq.condition.spatial$TOW_NO, unique(test.data$TOW_NO)),c("TOW_NO","START_LAT", "START_LONG", "ADJ_DEPTH","log.ctr.adj_depth")]
+livefreq.condition.spatial$YEAR <- surveyyear #add year to dataframe
+
+
+#Predict a meat weight for 100mm for just sampled tows; #re.form=NULL (all random effects included since only predicting on those tows that were sampled)
+
+livefreq.condition.spatial$Condition <- predict(MWTSHGM.YYYY,newdata=data.frame(Log.HEIGHT.CTR=rep(log(100), dim(livefreq.condition.spatial)[1])-mean(test.data$Log.HEIGHT),
+                                    Log.DEPTH.CTR=livefreq.condition.spatial$log.ctr.adj_depth, 
+                                    TOW_NO=livefreq.condition.spatial$TOW_NO),
+                                    re.form=NULL, type="response")
+
+head(livefreq.condition.spatial)
+
+#export for spatial plot later 
+write.csv(livefreq.condition.spatial, paste0(path.directory, assessmentyear, "/Assessment/Data/SurveyIndices/SPA",area,"/GMConditionforMap",surveyyear,".csv"))
+
+
+# ---- For Condition Time Series Figure ----
+
+#mean.depth <- read.csv('Y:/INSHORE SCALLOP/BoF/StandardDepth/BoFMeanDepths.csv') #File for the constant depth to predict on by area
+#DEFINE DEPTH:
+#-54.74 is for VMS INSIDE AREA
+#-56.65 is for VMS OUTER AREA
+
+
+#Bring in file with depths by area, note some are by strata groups within area
+mean.depth <- read.csv('Y:/INSHORE SCALLOP/BoF/StandardDepth/BoFMeanDepths.csv')[ ,c("AREA", "MeanDepth_m")] #File for the constant depth to predict on by area
+unique(mean.depth$AREA)
+length(mean.depth$AREA)
+
+mean.depth.GM <- mean.depth[mean.depth$AREA %in% c("SPA6 Modelled Area VMS IN",
+                                                  "SPA6 OUT"),]
+dim(mean.depth.GM)[1] == 2
+unique(mean.depth.GM$AREA)
+
+
+#create name column that matches condition file: 
+mean.depth.GM$AREA_NAME <- NA
+mean.depth.GM$AREA_NAME[mean.depth.GM$AREA == "SPA6 Modelled Area VMS IN"] <- "INVMS"
+mean.depth.GM$AREA_NAME[mean.depth.GM$AREA == "SPA6 OUT"] <- "OUTVMS"
+
+#add year, condition column 
+mean.depth.GM$YEAR <- surveyyear
+mean.depth.GM$Condition <- NA
+
+for (i in 1:length(mean.depth.GM$AREA)){
+  mean.depth.GM$Condition[i] <- predict(MWTSHGM.YYYY, newdata = data.frame(Log.HEIGHT.CTR=log(100) - mean(test.data$Log.HEIGHT),
+                                                                           Log.DEPTH.CTR = log(abs(mean.depth.GM$MeanDepth_m[i])) - mean(test.data$Log.DEPTH)),
+                                        re.form = NA, type = "response")  
+}
+mean.depth.GM
+mean.depth.GM$Condition <- round(mean.depth.GM$Condition,3)
+
+
+#Import previous year condition file: 
+#note contains IN and OUT VMS strata condition; they don't vary much since the only predictor is depth and SH is constant at 100mm 
+GM.con.ts <- read.csv(paste0(path.directory, assessmentyear-1,"/Assessment/Data/SurveyIndices/SPA6/SPA6_ConditionTimeSeries.csv"))
+
+#GM.con.ts <- GM.con.ts[GM.con.ts$YEAR!=2019,] 
+
+
+#update timeseries and write out new file: 
+GM.con.ts <- rbind(GM.con.ts %>% select(YEAR, STRATA, CONDITION), mean.depth.GM %>% select("YEAR",STRATA="AREA_NAME",CONDITION="Condition"))
+
+GM.con.ts <- GM.con.ts[order(GM.con.ts$STRATA, GM.con.ts$YEAR),]
+GM.con.ts
+GM.con.ts$CONDITION <- round(GM.con.ts$CONDITION,3)
+
+write.csv(GM.con.ts, paste0(path.directory, assessmentyear, "/Assessment/Data/SurveyIndices/SPA",area,"/SPA6_ConditionTimeSeries.csv"))
+
+
+#... Plot Condition Time Series Figure:
+
+condition.ts.plot <- ggplot(GM.con.ts,aes(x=YEAR, y=CONDITION,group_by(STRATA), color=STRATA)) +  
+  geom_line(aes(linetype=STRATA)) + geom_point(aes(shape=STRATA)) + 
+  scale_linetype_manual(values=c("dotted", "twodash")) + 
+  scale_shape_manual(values=c(1,6)) + 
+  scale_color_manual(values=c("black","gray34" )) + 
+  xlab("Year") + ylab("Condition (meat weight, g)") + theme_bw() + 
+  theme(legend.position = c(.8, .99),
+        legend.justification = c("left", "top"),
+        legend.box.just = "left",
+        legend.margin = margin(6, 6, 6, 6)) 
+condition.ts.plot
+
+#Export plot 
+png(paste0(path.directory, assessmentyear, "/Assessment/Figures/SPA6_conditionTrends.png" ))
+condition.ts.plot
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
