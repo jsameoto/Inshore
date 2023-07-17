e06655c8a2f6f02c06489b373821ea6ee89dc434
wbilson <brittany.wilson@dfo-mpo.gc.ca>
2023-07-17 09:44:34 -0300

    updated for BI2023

--- Standardize_depth.R
+++ Standardize_depth.R
@@ -34,8 +34,8 @@ for(fun in funcs)
 chan <- dbConnect(dbDriver("Oracle"),username=uid, password=pwd,'ptran')
 
 #set survey.year and cruise - *Note: requires single quotations within double quotations*
-survey.year <- "'2022'"
-cruise <- "'SFA292022'"
+survey.year <- "'2023'"
+cruise <- "'BI2023'"
 #appendingfile_year <- "2021" # for importing the current spreadsheet to append to.
 #updatefile_year <- "2021" #For saving file
 
